06ce7754eb60511e7b4a07d7cd23c43566c3ab94
root <cndtjq97@gamil.com>
2021-06-28 06:15:37 +0000

    upload main files

---
+++ ecg.py
@@ -0,0 +1,480 @@
+from torchvision.datasets.vision import VisionDataset
+from torchvision.datasets import MNIST
+import warnings
+from PIL import Image
+import os.path
+from typing import Any, Callable, Dict, IO, List, Optional, Tuple, Union
+
+import os
+import torch
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+from torchvision import transforms
+from tqdm import tqdm
+
+from time import time as t
+
+from bindsnet.encoding import Encoder, NullEncoder, PoissonEncoder
+from bindsnet.models import DiehlAndCook2015
+from bindsnet.network.monitors import Monitor
+from bindsnet.utils import get_square_weights, get_square_assignments
+from bindsnet.evaluation import (
+    all_activity,
+    proportion_weighting,
+    assign_labels,
+)
+from bindsnet.analysis.plotting import (
+    plot_input,
+    plot_spikes,
+    plot_weights,
+    plot_assignments,
+    plot_performance,
+    plot_voltages,
+    )
+
+interV	= 1 #128 * 10 * 1
+classN	= 6
+trainN	= 10000
+testN	= 4040
+timeT   = 1280
+
+class ECG(VisionDataset):
+    training_file = './training_t02mc6.pt'
+    test_file = './test_t02mc6.pt'
+    classes = ['0 - zero', '1 - one', '2 - two', '3 - three', '4 - four',
+               '5 - five']
+    
+    @property
+    def train_labels(self):
+        warnings.warn("train_labels has been renamed targets")
+        return self.targets
+
+    @property
+    def test_labels(self):
+        warnings.warn("test_labels has been renamed targets")
+        return self.targets
+
+    @property
+    def train_data(self):
+        warnings.warn("train_data has been renamed data")
+        return self.data
+
+    @property
+    def test_data(self):
+        warnings.warn("test_data has been renamed data")
+        return self.data
+
+    def __init__(
+            self,
+            root: str,
+            train: bool = True,
+            transform: Optional[Callable] = None,
+            target_transform: Optional[Callable] = None,
+            download: bool = False,
+    ) -> None:
+        super(ECG, self).__init__(root, transform=transform,
+                                    target_transform=target_transform)
+        self.train = train  # training set or test set
+
+        if download:
+            pass
+
+        if not self._check_exists():
+            raise RuntimeError('Dataset not found.' +
+                               ' You can use download=True to download it')
+        if self.train:
+            data_file = self.training_file
+        else:
+            data_file = self.test_file
+        self.data, self.targets = torch.load(os.path.join(self.processed_folder, data_file))
+        #self.data = list(self.data)
+        #self.targets = list(self.targets)
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], int(self.targets[index])
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        # img = Image.fromarray(img.numpy(), mode='L')
+
+        #if self.transform is not None:
+        #    img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    @property
+    def processed_folder(self) -> str:
+        return os.path.join(self.root, self.__class__.__name__, 'processed')
+
+    def _check_exists(self) -> bool:
+        return (os.path.exists(os.path.join(self.processed_folder,
+                                            self.training_file)) and
+                os.path.exists(os.path.join(self.processed_folder,
+                                            self.test_file)))
+
+class TorchvisionDatasetWrapper(ECG):
+    def __init__(
+        self,
+        image_encoder: Optional[Encoder] = None,
+        label_encoder: Optional[Encoder] = None,
+        *args,
+        **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+
+        self.args = args
+        self.kwargs = kwargs
+
+        # Allow the passthrough of None, but change to NullEncoder
+        if image_encoder is None:
+            image_encoder = NullEncoder()
+
+        if label_encoder is None:
+            label_encoder = NullEncoder()
+
+        self.image_encoder = image_encoder
+        self.label_encoder = label_encoder
+
+    def __getitem__(self, ind: int) -> Dict[str, torch.Tensor]:
+        image, label = super().__getitem__(ind)
+
+        output = {
+            "image": image,
+            "label": label,
+            "encoded_image": self.image_encoder(image),
+            "encoded_label": self.label_encoder(label),
+        }
+
+        return output
+
+    def __len__(self):
+        return super().__len__()
+
+
+#byECG = TorchvisionDatasetWrapper("ECG")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--seed", type=int, default=0)
+parser.add_argument("--n_neurons", type=int, default=100)
+parser.add_argument("--n_epochs", type=int, default=1)
+parser.add_argument("--n_test", type=int, default=testN)
+parser.add_argument("--n_train", type=int, default=trainN)
+parser.add_argument("--n_workers", type=int, default=-1)
+parser.add_argument("--exc", type=float, default=22.5)
+parser.add_argument("--inh", type=float, default=120)
+parser.add_argument("--theta_plus", type=float, default=0.05)
+parser.add_argument("--time", type=int, default=timeT)
+parser.add_argument("--dt", type=int, default=1.0)
+parser.add_argument("--intensity", type=float, default=128)
+parser.add_argument("--progress_interval", type=int, default=10)
+parser.add_argument("--update_interval", type=int, default=250)
+parser.add_argument("--train", dest="train", action="store_true")
+parser.add_argument("--test", dest="train", action="store_false")
+parser.add_argument("--plot", dest="plot", action="store_true")
+parser.add_argument("--gpu", dest="gpu", action="store_true")
+parser.set_defaults(plot=False, gpu=True)
+
+args = parser.parse_args()
+
+seed = args.seed
+n_neurons = args.n_neurons
+n_epochs = args.n_epochs
+n_test = args.n_test
+n_train = args.n_train
+n_workers = args.n_workers
+exc = args.exc
+inh = args.inh
+theta_plus = args.theta_plus
+time = args.time
+dt = args.dt
+intensity = args.intensity
+progress_interval = args.progress_interval
+update_interval = args.update_interval
+train = args.train
+plot = args.plot
+gpu = args.gpu
+
+# Sets up Gpu use
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if gpu and torch.cuda.is_available():
+    torch.cuda.manual_seed_all(seed)
+else:
+    torch.manual_seed(seed)
+    device = "cpu"
+    if gpu:
+        gpu = False
+
+torch.set_num_threads(os.cpu_count() - 1)
+print("Running on Device = ", device)
+
+# Determines number of workers to use
+if n_workers == -1:
+    n_workers = gpu * 4 * torch.cuda.device_count()
+
+#if not train:
+#    update_interval = n_test
+
+n_sqrt = int(np.ceil(np.sqrt(n_neurons)))
+start_intensity = intensity
+
+# Build network.
+network = DiehlAndCook2015(
+    n_inpt=interV,
+    n_neurons=n_neurons,
+    exc=exc,
+    inh=inh,
+    dt=dt,
+    norm= 1,#interV / 10.0,
+    theta_plus=theta_plus,
+    inpt_shape=(1, interV),
+)
+
+# Directs network to GPU
+if gpu:
+    network.to("cuda")
+
+# Load MNIST data.
+train_dataset = TorchvisionDatasetWrapper(
+    None,    #PoissonEncoder(time=time, dt=dt),
+    None,
+    root=os.path.join("..", "..", "data", "ECG"),
+    download=True,
+    train=True,
+    transform=transforms.Compose(
+        [transforms.ToTensor()]
+    ),
+)
+
+
+# Record spikes during the simulation.
+spike_record = torch.zeros((update_interval, int(time / dt), n_neurons), device=device)
+
+# Neuron assignments and spike proportions.
+n_classes = classN
+assignments = -torch.ones(n_neurons, device=device)
+proportions = torch.zeros((n_neurons, n_classes), device=device)
+rates = torch.zeros((n_neurons, n_classes), device=device)
+
+# Sequence of accuracy estimates.
+accuracy = {"all": [], "proportion": []}
+
+# Voltage recording for excitatory and inhibitory layers.
+exc_voltage_monitor = Monitor(
+    network.layers["Ae"], ["v"], time=int(time / dt), device=device
+)
+inh_voltage_monitor = Monitor(
+    network.layers["Ai"], ["v"], time=int(time / dt), device=device
+)
+network.add_monitor(exc_voltage_monitor, name="exc_voltage")
+network.add_monitor(inh_voltage_monitor, name="inh_voltage")
+
+# Set up monitors for spikes and voltages
+spikes = {}
+for layer in set(network.layers):
+    spikes[layer] = Monitor(
+        network.layers[layer], state_vars=["s"], time=int(time / dt), device=device
+    )
+    network.add_monitor(spikes[layer], name="%s_spikes" % layer)
+
+voltages = {}
+for layer in set(network.layers) - {"X"}:
+    voltages[layer] = Monitor(
+        network.layers[layer], state_vars=["v"], time=int(time / dt), device=device
+    )
+    network.add_monitor(voltages[layer], name="%s_voltages" % layer)
+
+inpt_ims, inpt_axes = None, None
+spike_ims, spike_axes = None, None
+weights_im = None
+assigns_im = None
+perf_ax = None
+voltage_axes, voltage_ims = None, None
+
+# Train the network.
+print("\nBegin training.\n")
+start = t()
+labels = []
+for epoch in range(n_epochs):
+
+    if epoch % progress_interval == 0:
+        print("Progress: %d / %d (%.4f seconds)" % (epoch, n_epochs, t() - start))
+        start = t()
+
+    # Create a dataloader to iterate and batch data
+    dataloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=1, shuffle=True, num_workers=n_workers, pin_memory=gpu
+    )
+
+    for step, batch in enumerate(tqdm(dataloader)):
+        if step > n_train:
+            break
+        # Get next input sample.
+        inputs = {"X": batch["image"].view(int(time / dt), 1, 1, interV)}
+        if gpu:
+            inputs = {k: v.cuda() for k, v in inputs.items()}
+
+        if step % update_interval == 0 and step > 0:
+            # Convert the array of labels into a tensor
+            label_tensor = torch.tensor(labels, device=device)
+
+            # Get network predictions.
+            all_activity_pred = all_activity(
+                spikes=spike_record,
+                assignments=assignments,
+                n_labels=n_classes,
+            )
+            proportion_pred = proportion_weighting(
+                spikes=spike_record,
+                assignments=assignments,
+                proportions=proportions,
+                n_labels=n_classes,
+            )
+
+            # Compute network accuracy according to available classification strategies.
+            accuracy["all"].append(
+                100
+                * torch.sum(label_tensor.long() == all_activity_pred).item()
+                / len(label_tensor)
+            )
+            accuracy["proportion"].append(
+                100
+                * torch.sum(label_tensor.long() == proportion_pred).item()
+                / len(label_tensor)
+            )
+
+            print(
+                "\nAll activity accuracy: %.2f (last), %.2f (average), %.2f (best)"
+                % (
+                    accuracy["all"][-1],
+                    np.mean(accuracy["all"]),
+                    np.max(accuracy["all"]),
+                )
+            )
+            print(
+                "Proportion weighting accuracy: %.2f (last), %.2f (average), %.2f"
+                " (best)\n"
+                % (
+                    accuracy["proportion"][-1],
+                    np.mean(accuracy["proportion"]),
+                    np.max(accuracy["proportion"]),
+                )
+            )
+
+            # Assign labels to excitatory layer neurons.
+            assignments, proportions, rates = assign_labels(
+                spikes=spike_record,
+                labels=label_tensor,
+                n_labels=n_classes,
+                rates=rates,
+            )
+
+            labels = []
+
+        labels.append(batch["label"])
+        
+        # Run the network on the input.
+        network.run(inputs=inputs, time=time, input_time_dim=1)
+
+        # Get voltage recording.
+        exc_voltages = exc_voltage_monitor.get("v")
+        inh_voltages = inh_voltage_monitor.get("v")
+
+        # Add to spikes recording.
+        spike_record[step % update_interval] = spikes["Ae"].get("s").squeeze()
+        
+        spnum=spike_record.sum(dim=1)
+        spnum=spnum.sum(dim=1)
+
+        #print(inputs)
+        print("input: ",inputs['X'].sum(),"    label: ",labels[step % update_interval], "   spikes: ",spnum[step % update_interval])
+
+        network.reset_state_variables()  # Reset state variables.
+
+print("Progress: %d / %d (%.4f seconds)" % (epoch + 1, n_epochs, t() - start))
+print("Training complete.\n")
+
+
+# Load MNIST data.
+test_dataset = TorchvisionDatasetWrapper(
+    None, #PoissonEncoder(time=time, dt=dt),
+    None,
+    root=os.path.join("..", "..", "data", "ECG"),
+    download=True,
+    train=False,
+    transform=transforms.Compose(
+        [transforms.ToTensor()]#, transforms.Lambda(lambda x: x * intensity)]
+    ),
+)
+
+# Sequence of accuracy estimates.
+accuracy = {"all": 0, "proportion": 0}
+
+# Record spikes during the simulation.
+spike_record = torch.zeros((1, int(time / dt), n_neurons), device=device)
+
+# Train the network.
+print("\nBegin testing\n")
+network.train(mode=False)
+start = t()
+
+pbar = tqdm(total=n_test)
+for step, batch in enumerate(test_dataset):
+    if step > n_test:
+        break
+    # Get next input sample.
+    inputs = {"X": batch["encoded_image"].view(int(time / dt), 1, 1, interV)}
+    if gpu:
+        inputs = {k: v.cuda() for k, v in inputs.items()}
+
+    # Run the network on the input.
+    network.run(inputs=inputs, time=time, input_time_dim=1)
+
+    # Add to spikes recording.
+    spike_record[0] = spikes["Ae"].get("s").squeeze()
+
+    # Convert the array of labels into a tensor
+    label_tensor = torch.tensor(batch["label"], device=device)
+
+    # Get network predictions.
+    all_activity_pred = all_activity(
+        spikes=spike_record, assignments=assignments, n_labels=n_classes
+    )
+    proportion_pred = proportion_weighting(
+        spikes=spike_record,
+        assignments=assignments,
+        proportions=proportions,
+        n_labels=n_classes,
+    )
+
+    # Compute network accuracy according to available classification strategies.
+    accuracy["all"] += float(torch.sum(label_tensor.long() == all_activity_pred).item())
+    accuracy["proportion"] += float(
+        torch.sum(label_tensor.long() == proportion_pred).item()
+    )
+
+    network.reset_state_variables()  # Reset state variables.
+    pbar.set_description_str("Test progress: ")
+    pbar.update()
+
+print("\nAll activity accuracy: %.2f" % (accuracy["all"] / n_test))
+print("Proportion weighting accuracy: %.2f \n" % (accuracy["proportion"] / n_test))
+
+
+print("Progress: %d / %d (%.4f seconds)" % (epoch + 1, n_epochs, t() - start))
+print("Testing complete.\n")
+
